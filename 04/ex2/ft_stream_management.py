eb3d11d0f009e495ee03d258d415c106db6289a1
Noel Caravaca Gil <ncaravac@c1r16s6.42madrid.com>
2026-02-06 15:14:38 +0100

    Updated: 04 Completed

--- 04/ex2/ft_stream_management.py
+++ 04/ex2/ft_stream_management.py
@@ -1,17 +1,26 @@
-"""Docstrings"""
+"""Module for managing triple-channel archive communications"""
 
 
 import sys
 
+
 def main():
+    """
+    Handles archivist status reporting.
+    
+    Processes user input and demonstrates communication via standard input, 
+    standard output, and standard error streams.
+    """
     print("=== CYBER ARCHIVES - COMUNICATION SYSTEM ===\n")
 
     archivist_id = input("Input Stream active. Enter archivist ID: ")
     status_report = input("Input Stream active. Enter status report: ")
 
-    sys.stdout.write(f"\n[STANDARD] Archive status from {archivist_id}: {status_report}\n")
-    sys.stderr.write("[ALERT] System diagnostic: Comunication channels verified\n")
-    sys.stdout.write("[STANDARD] Data transmission complete\n")
+    sys.stdout.write("\n{[}STANDARD{]} Archive status from "
+                     f" {archivist_id}: {status_report}\n")
+    sys.stderr.write("{[}ALERT{]} System diagnostic: "
+                     "Comunication channels verified\n")
+    sys.stdout.write("{[}STANDARD{]} Data transmission complete\n")
 
     sys.stdout.write("\nThree-channel communication test successful.\n")
 
